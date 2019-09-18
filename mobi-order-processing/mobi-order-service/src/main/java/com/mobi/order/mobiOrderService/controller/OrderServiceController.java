330fe0435f820837835e924dd5003f9ef53fd4f1
Soham <sohamchaudhary@gmail.com>
2019-09-18 17:56:41 +0530

    Adding PostgreSQL DB and some minor changes

--- mobi-order-processing/mobi-order-service/src/main/java/com/mobi/order/mobiOrderService/controller/OrderServiceController.java
+++ mobi-order-processing/mobi-order-service/src/main/java/com/mobi/order/mobiOrderService/controller/OrderServiceController.java
@@ -2,13 +2,16 @@ package com.mobi.order.mobiOrderService.controller;
 
 import com.mobi.order.mobiOrderService.entities.OrderDetails;
 import com.mobi.order.mobiOrderService.models.PlaceOrderReqDto;
+import com.mobi.order.mobiOrderService.repository.OrderRepository;
 import com.mobi.order.mobiOrderService.services.impl.OrderServiceImpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class OrderServiceController {
@@ -16,18 +19,36 @@ public class OrderServiceController {
     @Autowired
     private OrderServiceImpl orderServiceImpl;
 
-    @RequestMapping("/getOrder/{orderId}")
-    public OrderDetails getOrder(@PathVariable  Long orderId){
-        return  orderServiceImpl.getOrder(orderId);
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @GetMapping("/test")
+    public String testMessage() {
+        OrderDetails o1 = new OrderDetails();
+        o1.setOrderStatus("complete");
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date date = new Date();
+        o1.setOrderTime(date);
+        Map<String, Integer> map = new HashMap<>();
+        map.put("Pen", 10);
+        o1.setProductQtyMap(map);
+        if (o1 != null)
+            orderRepository.save(o1);
+        return "Added successfully";
     }
 
-    @RequestMapping(method= RequestMethod.POST, value = "/placeOrder")
-    public Long placeOrder(PlaceOrderReqDto placeOrderReqDto){
+    @RequestMapping("/getOrder/{orderId}")
+    public OrderDetails getOrder(@PathVariable Long orderId) {
+        return orderServiceImpl.getOrder(orderId);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/placeOrder")
+    public Long placeOrder(PlaceOrderReqDto placeOrderReqDto) {
         return orderServiceImpl.placeOrder(placeOrderReqDto);
     }
 
     @RequestMapping("/getOrderStatus/{orderId}")
-    public String getOrderStatus(@PathVariable  Long orderId){
-        return  orderServiceImpl.getOrderStatus(orderId);
+    public String getOrderStatus(@PathVariable Long orderId) {
+        return orderServiceImpl.getOrderStatus(orderId);
     }
 }
--- mobi-order-processing/mobi-order-service/src/main/java/com/mobi/order/mobiOrderService/repository/OrderRepository.java
+++ mobi-order-processing/mobi-order-service/src/main/java/com/mobi/order/mobiOrderService/repository/OrderRepository.java
@@ -2,9 +2,8 @@ package com.mobi.order.mobiOrderService.repository;
 
 import com.mobi.order.mobiOrderService.entities.OrderDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface OrderRepository extends JpaRepository<OrderDetails, Long> {
+
     OrderDetails findByOrderId(long orderId);
 }
