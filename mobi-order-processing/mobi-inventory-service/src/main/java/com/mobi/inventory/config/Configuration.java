50d7245e37044da8a35782656990572a2fa0d903
hrishikesh.tele <hrishikesh.tele@mobiliya.com>
2019-09-24 10:42:03 +0530

    Modified update method and added swagger

--- mobi-order-processing/mobi-inventory-service/src/main/java/com/mobi/inventory/config/Configuration.java
+++ mobi-order-processing/mobi-inventory-service/src/main/java/com/mobi/inventory/config/Configuration.java
@@ -2,6 +2,11 @@ package com.mobi.inventory.config;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * The type Configuration.
@@ -18,4 +23,18 @@ public class Configuration {
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
+
+    @EnableSwagger2
+    public class SwaggerConfig {
+        @Bean
+        public Docket api() {
+            return new Docket(DocumentationType.SWAGGER_2)
+                    .select()
+                    .apis(RequestHandlerSelectors.any())
+                    .paths(PathSelectors.any())
+                    .build();
+        }
+    }
 }
+
+
