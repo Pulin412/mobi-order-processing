e392a8613a16a072a343220c5493a272a231605f
hrishikesh.tele <hrishikesh.tele@mobiliya.com>
2019-10-03 16:05:07 +0530

    Added docker file for other modules

--- mobi-order-processing/mobi-customer-service/src/main/java/com/mobi/customer/MobiCustomerService/MobiCustomerServiceApplication.java
+++ mobi-order-processing/mobi-customer-service/src/main/java/com/mobi/customer/MobiCustomerService/MobiCustomerServiceApplication.java
@@ -2,12 +2,12 @@ package com.mobi.customer.MobiCustomerService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MobiCustomerServiceApplication {
-
-
-  public static void main(String[] args) {
-    SpringApplication.run(MobiCustomerServiceApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(MobiCustomerServiceApplication.class, args);
+    }
 }
